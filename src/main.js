3d4f3d2f06c4da97607adcfb53d8427417164f48
Calvin <calvincgill@gmail.com>
2020-05-10 11:49:19 +0100

    Moved 'components' folder logic into it's own function, now we can just check the response (if that fodler exists/has been created) and handle accordingly.

--- src/main.js
+++ src/main.js
@@ -1,19 +1,42 @@
 import fs from 'fs'
+import inquirer from 'inquirer'
 import chalk from 'chalk'
 import { promisify } from 'util'
 
 const access = promisify(fs.access)
 
-export async function constructComponent (options) {
+/* Used to handle 'components' folder creation/checking */
+async function handleComponentsDirectory () {
   try {
     await access('./components', fs.constants.R_OK)
+    return true
+  } catch {
+    const questions = []
+    questions.push({
+      type: 'confirm',
+      name: 'componentsDirectory',
+      message:
+        "'components' directory does not exist, do you want to create it?"
+    })
+    const answers = await inquirer.prompt(questions)
+    if (answers['componentsDirectory']) {
+      fs.mkdirSync('./components')
+      console.log(`${chalk.red.green('DONE')}: 'components' folder created.`)
+    }
+    return answers['componentsDirectory']
+  }
+}
+
+export async function constructComponent (options) {
+  try {
+    let answer = await handleComponentsDirectory()
+    // Check answer, handle accordingly
   } catch {
     console.log(
       `${chalk.red.bold('ERROR')}: 'components' folder does not exist.`
     )
     process.exit(1)
   }
-
   console.log(
     `${chalk.green.bold('Component Created')}: ${options.componentName}`
   )
