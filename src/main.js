27f7afdd8222520c7ac30f17e2ceea0b74cb7dbc
Calvin <calvincgill@gmail.com>
2020-05-10 12:47:08 +0100

    Now able to create the needed files in the correct directory, might want to create a 'templates/' folder that contains the various templates we can copy.

--- src/main.js
+++ src/main.js
@@ -21,23 +21,77 @@ async function handleComponentsDirectory () {
     const answers = await inquirer.prompt(questions)
     if (answers['componentsDirectory']) {
       fs.mkdirSync('./components')
-      console.log(`${chalk.red.green('DONE')}: 'components' folder created.`)
+      console.log(`${chalk.green.bold('DONE')}: 'components' folder created.`)
     }
     return answers['componentsDirectory']
   }
 }
 
+async function handleComponentCreation (componentExists, component) {
+  if (componentExists) {
+    const questions = []
+    questions.push({
+      type: 'confirm',
+      name: 'createComponent',
+      message: `component '${component}' already exists, do you want to over-write it?`
+    })
+    const answers = await inquirer.prompt(questions)
+    return answers['createComponent']
+  } else {
+    fs.mkdirSync(`./components/${component}`)
+    /* Handles file creation (needs to be moved into it's own function) */
+    fs.writeFile(
+      `./components/${component}/${component}.component.jsx`,
+      '',
+      function (error) {
+        if (error !== null) {
+          console.log(error)
+        }
+      }
+    )
+    fs.writeFile(
+      `./components/${component}/${component}.styles.css`,
+      '',
+      function (error) {
+        if (error !== null) {
+          console.log(error)
+        }
+      }
+    )
+    return true
+  }
+}
+
 export async function constructComponent (options) {
   try {
-    let answer = await handleComponentsDirectory()
-    // Check answer, handle accordingly
+    var componentsDirectoryExists = await handleComponentsDirectory()
   } catch {
     console.log(
-      `${chalk.red.bold('ERROR')}: 'components' folder does not exist.`
+      `${chalk.red.bold(
+        'ERROR'
+      )}: 'components' could not be created/does not exist.`
     )
     process.exit(1)
   }
-  console.log(
-    `${chalk.green.bold('Component Created')}: ${options.componentName}`
-  )
+
+  if (!componentsDirectoryExists) {
+    process.exit(1)
+  } else {
+    const componentExists = fs.existsSync(
+      `./components/${options.componentName}`
+    )
+    const createComponent = await handleComponentCreation(
+      componentExists,
+      options.componentName
+    )
+    if (!createComponent) {
+      process.exit(1)
+    } else {
+      console.log(
+        `${chalk.green.bold('DONE')}: '${
+          options.componentName
+        }' component created.`
+      )
+    }
+  }
 }
